2fd231c70d12cdbbdb2e3ab55a3ec42ff8e63634
Nebojša Zoraja <nebojsa.zoki@gmail.com>
2022-03-11 20:55:30 +0100

    signout dodat

--- Menza/screens/LoginScreen.js
+++ Menza/screens/LoginScreen.js
@@ -6,7 +6,7 @@ import {
   View,
   TouchableOpacity,
 } from 'react-native';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import auth from '@react-native-firebase/auth';
 
 const LoginScreen = () => {
@@ -14,27 +14,38 @@ const LoginScreen = () => {
   const [password, setPassword] = useState('');
 
   const register = () => {
-    auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then(userCred => {
-        alert(userCred.user.email);
-      })
-      .catch(error => {
-        alert(error.message);
-      });
+    if (email != null && email.length != 0 && email != '' && password != null && password.length != 0 && password != '') {
+      auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(userCred => {
+          alert(userCred.user.email);
+        })
+        .catch(error => {
+          alert(error.message);
+        });
+    } else {
+      alert("Please type your email and password");
+    }
+
   };
+
+  const signOut = () => {
+    auth().signOut().then(() => alert("Sign-out uspesan!")).catch(() => alert("Not signed in"));
+  }
 
   return (
     <ScrollView contentContainerStyle={styles.container} behavior="padding">
       <View style={styles.inputContainer}>
         <TextInput
           placeholder="Email"
+          placeholderTextColor="#C7C7CD"
           value={email}
           onChangeText={text => setEmail(text)}
           style={styles.input}
         />
         <TextInput
           placeholder="Password"
+          placeholderTextColor="#C7C7CD"
           value={password}
           onChangeText={text => setPassword(text)}
           style={styles.input}
@@ -49,6 +60,11 @@ const LoginScreen = () => {
           onPress={register}
           style={[styles.button, styles.buttonOutline]}>
           <Text style={styles.buttonOutlineText}>Register</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={signOut}
+          style={[styles.button, styles.buttonOutlineRed]}>
+          <Text style={styles.buttonOutlineTextRed}>Sign Out</Text>
         </TouchableOpacity>
       </View>
     </ScrollView>
@@ -68,6 +84,7 @@ const styles = StyleSheet.create({
   },
   input: {
     backgroundColor: 'white',
+    color: 'black',
     paddingHorizontal: 15,
     paddingVertical: 10,
     borderRadius: 10,
@@ -92,6 +109,13 @@ const styles = StyleSheet.create({
     borderColor: '#0782F9',
     borderWidth: 2,
   },
+  buttonOutlineRed: {
+    backgroundColor: 'red',
+    color: 'red',
+    marginTop: 5,
+    borderColor: 'red',
+    borderWidth: 2,
+  },
   buttonText: {
     color: 'white',
     fontWeight: '700',
@@ -99,6 +123,11 @@ const styles = StyleSheet.create({
   },
   buttonOutlineText: {
     color: '#0782F9',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  buttonOutlineTextRed: {
+    color: 'white',
     fontWeight: '700',
     fontSize: 16,
   },
